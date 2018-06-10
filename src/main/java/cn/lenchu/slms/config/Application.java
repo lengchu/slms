a607ec99d3e123198ff36dea355d3c6952fb4ad5
lenchu <553308706@qq.com>
2018-06-10 14:02:21 +0800

    添加跨域配置

---
+++ src/main/java/cn/lenchu/slms/config/Application.java
@@ -0,0 +1,21 @@
+package cn.lenchu.slms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class Application extends WebMvcConfigurerAdapter {
+
+    /**
+     * 跨域配置
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
+}
